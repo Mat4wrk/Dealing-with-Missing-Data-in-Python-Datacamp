fabc55c4fb5e342d9b2d49aa4726b6e840770060
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 22:12:49 +0700

    Create Mean & median imputation.py

---
+++ 3.Imputation Techniques/Mean & median imputation.py
@@ -0,0 +1,21 @@
+Create a SimpleImputer() object while performing mean imputation.
+Impute the copied DataFrame.
+# Make a copy of diabetes
+diabetes_mean = diabetes.copy(deep=True)
+
+# Create mean imputer object
+mean_imputer = SimpleImputer(strategy='mean')
+
+# Impute mean values in the DataFrame diabetes_mean
+diabetes_mean.iloc[:, :] = mean_imputer.fit_transform(diabetes_mean)
+
+Create a SimpleImputer() object while performing median imputation.
+Impute the copied DataFrame.
+# Make a copy of diabetes
+diabetes_median = diabetes.copy(deep=True)
+
+# Create median imputer object
+median_imputer = SimpleImputer(strategy='median')
+
+# Impute median values in the DataFrame diabetes_median
+diabetes_median.iloc[:, :] = median_imputer.fit_transform(diabetes_median)
