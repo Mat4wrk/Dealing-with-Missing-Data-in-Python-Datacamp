d726bb8e3583db843685cb027ee26667cf94b1d6
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 14:48:54 +0700

    Delete Does Missingness Have A Pattern? directory

--- Does Missingness Have A Pattern?/Guess the missingness type.py
+++
@@ -1,9 +0,0 @@
-# Import missingno as msno
-import missingno as msno
-
-# Visualize the missingness summary
-msno.matrix(diabetes)
-
-# Display nullity matrix
-display("/usr/local/share/datasets/matrix_diabetes.png")
-
