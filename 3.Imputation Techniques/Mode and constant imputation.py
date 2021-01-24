83e048ee7a5c019eeafa1968a369b3d47b26be1d
Mat4wrk <77670765+Mat4wrk@users.noreply.github.com>
2021-01-24 22:16:23 +0700

    Create Mode and constant imputation.py

---
+++ 3.Imputation Techniques/Mode and constant imputation.py
@@ -0,0 +1,21 @@
+Create a SimpleImputer() object while performing mode (or most frequent) imputation.
+Impute the copied DataFrame.
+# Make a copy of diabetes
+diabetes_mode = diabetes.copy(deep=True)
+
+# Create mode imputer object
+mode_imputer = SimpleImputer(strategy='most_frequent')
+
+# Impute using most frequent value in the DataFrame mode_imputer
+diabetes_mode.iloc[:, :] = mode_imputer.fit_transform(diabetes_mode)
+
+Create a SimpleImputer() object while filling missing values to 0.
+Impute the copied DataFrame.
+# Make a copy of diabetes
+diabetes_constant = diabetes.copy(deep=True)
+
+# Create median imputer object
+constant_imputer = SimpleImputer(strategy='constant', fill_value=0)
+
+# Impute missing values to 0 in diabetes_constant
+diabetes_constant.iloc[:, :] = constant_imputer.fit_transform(diabetes_constant)
